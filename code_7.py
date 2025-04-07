2e993de70630e1b8f10d69bbcbfc27189f43f895
octaviogumo07 <Ogutierrez5@miners.utep.edu>
2025-04-06 21:47:35 -0600

    code 7 with stats and graph

--- code_7.py
+++ code_7.py
@@ -1,8 +1,40 @@
 import pandas as pd
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
+import seaborn as sns
 #If any of this libraries is missing from your computer. Please install them using pip.
 
 filename = 'Flight_Delays_2018.csv'
-
+df = pd.read_csv(filename)
 #ARR_DELAY is the column name that should be used as dependent variable (Y).
+
+#selecting relevant columns for delay analysis
+delay_columns = ["ARR_DELAY", "DEP_DELAY", "TAXI_OUT", "TAXI_IN", "AIR_TIME", "DISTANCE", "CARRIER_DELAY", "WEATHER_DELAY", "NAS_DELAY", "SECURITY_DELAY", "LATE_AIRCRAFT_DELAY"]
+
+#dropping rows with missing values
+df_clean = df[delay_columns].dropna()
+
+#descriptive analytics
+#correlation heatmap
+plt.figure(figsize=(12,8))
+corr_matrix = df_clean.corr()
+sns.heatmap(corr_matrix, annot=True, fmt=".2f", cmap='coolwarm')
+plt.title("Correlation Matrix of Flight Delay Related Variables")
+plt.tight_layout()
+plt.savefig("descriptive_correlation_heatmap.png")
+plt.close()
+
+#predictive analyticis
+#select predictors based on correlation with ARR_DELAY
+predictors = ['DEP_DELAY', 'CARRIER_DELAY', 'LATE_AIRCRAFT_DELAY', 'WEATHER_DELAY', 'NAS_DELAY']
+X = df_clean[predictors]
+y = df_clean['ARR_DELAY']
+
+#add constant for intercception
+x = sm.add_constant(X)
+
+#fit linear regression model
+model = sm.OLS(y, X).fit()
+
+with open("predictive_model_summary.txt", "w") as f:
+    f.write(str(model.summary()))
